44c0365f73f176e39bf41de694dbeb49a1700f42
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2024-04-13 21:42:32 +0200

    Extract thumbnails from target points

--- src/sign_prosody_extraction/cli.py
+++ src/sign_prosody_extraction/cli.py
@@ -19,11 +19,14 @@ def load_video (video_file):
 @click.option('--track-video/--no-track-video', default=False, help='Output a video with the extracted tracks')
 @click.option('--targets/--no-targets', 'find_targets', default=True, help='Find target points')
 @click.option('--plot/--no-plot', default=False, help='Output a plot with the extracted prosody')
-def main(videos, algorithm, track_video, find_targets, plot):
+@click.option('--thumbnails', type=str, help='''Generate thumbnails at the specified frames. Can be FIRST, LAST, ALL, or a list of frame numbers''')
+def main(videos, algorithm, track_video, find_targets, plot, thumbnails):
     if algorithm == 'cotracker':
         from .articulator.cotracker import track_hands
     elif algorithm == 'mediapipe':
         from .articulator.mediapipe import track_hands
+    if thumbnails:
+        find_targets = True
     from .plot import plot_prosody
     from .visualize import overlay_tracks
     from .targets import get_target_points
@@ -35,3 +38,21 @@ def main(videos, algorithm, track_video, find_targets, plot):
         targets = get_target_points(hands[0]) if find_targets else [] # For now only right hand
         if plot:
             plot_prosody(hands, "plot.png", points=targets)
+        if thumbnails:
+            get_thumbnails(video, targets, first_frame, thumbnails)
+
+
+def get_thumbnails(video, targets, first_frame, frames):
+    from torchvision.transforms import ToPILImage
+    if frames == 'FIRST':
+        frames = [targets[0]]
+    elif frames == 'LAST':
+        frames = [targets[-1]]
+    elif frames == 'ALL':
+        frames = targets
+    else:
+        frames = [targets[i] for i in frames]
+    to_pil = ToPILImage()
+    for i, f in enumerate(frames):
+        image = video[0, f+first_frame].transpose(1,2,0).astype(np.uint8)
+        to_pil(image).save(f"thumbnail_{i}.png")
