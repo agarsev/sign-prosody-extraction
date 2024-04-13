ef5798839e925db1f8bf1e7eaceadada227627d6
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2024-04-13 20:52:26 +0200

    Find target points

--- src/sign_prosody_extraction/cli.py
+++ src/sign_prosody_extraction/cli.py
@@ -14,18 +14,24 @@ def load_video (video_file):
 
 @click.command()
 @click.argument('videos', nargs=-1, type=click.Path(exists=True), required=True)
-@click.option('--visualize', is_flag=True, help='Visualize the extracted tracks')
 @click.option('--cotracker', 'algorithm', flag_value='cotracker', default=True, help='Use the CoTracker algorithm')
 @click.option('--mediapipe', 'algorithm', flag_value='mediapipe', help='Use the MediaPipe algorithm')
-def main(videos, visualize, algorithm):
+@click.option('--track-video/--no-track-video', default=False, help='Output a video with the extracted tracks')
+@click.option('--targets/--no-targets', 'find_targets', default=True, help='Find target points')
+@click.option('--plot/--no-plot', default=False, help='Output a plot with the extracted prosody')
+def main(videos, algorithm, track_video, find_targets, plot):
     if algorithm == 'cotracker':
         from .articulator.cotracker import track_hands
     elif algorithm == 'mediapipe':
         from .articulator.mediapipe import track_hands
     from .plot import plot_prosody
     from .visualize import overlay_tracks
+    from .targets import get_target_points
     for video_file in videos:
         video = load_video(video_file)
         hands, first_frame = track_hands(video)
-        plot_prosody(hands, "plot.png")
-        overlay_tracks(video[:, first_frame:], hands, "track.mp4")
+        if track_video:
+            overlay_tracks(video[:, first_frame:], hands, "track.mp4")
+        targets = get_target_points(hands[0]) if find_targets else [] # For now only right hand
+        if plot:
+            plot_prosody(hands, "plot.png", points=targets)
